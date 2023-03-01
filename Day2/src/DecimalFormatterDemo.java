e618e98a86da01b5795297bdfe66669055b034ac
Omkar Gujar <gujaromkar.pradip@boomi.com>
2023-03-02 00:22:08 +0530

    Adding Format

--- Day2/src/DecimalFormatterDemo.java
+++ Day2/src/DecimalFormatterDemo.java
@@ -8,10 +8,17 @@ public class DecimalFormatterDemo {
     }
 
     static public void main(String[] args) {
-
         customFormat("###,###.###", 123456.789);
         customFormat("###.##", 123456.789);
         customFormat("000000.000", 123.78);
         customFormat("$###,###.###", 12345.67);
+
+        double floatVar = 12.324;
+        int intVar = 100;
+        String stringVar = "Omkar";
+
+        System.out.format("The value of " + "the float variable is " +
+                "%.4f, while the value of the " + "integer variable is %d, " +
+                "and the string is %s", floatVar, intVar, stringVar);
     }
 }
