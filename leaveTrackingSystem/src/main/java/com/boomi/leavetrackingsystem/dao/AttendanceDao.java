f805f574eb8cfa874bd88706afa5466a5f698c60
Omkar Gujar <gujaromkar.pradip@boomi.com>
2023-04-16 02:05:56 +0530

    Adding deleteAttendanceByDate & seeProfessor

--- leaveTrackingSystem/src/main/java/com/boomi/leavetrackingsystem/dao/AttendanceDao.java
+++ leaveTrackingSystem/src/main/java/com/boomi/leavetrackingsystem/dao/AttendanceDao.java
@@ -2,7 +2,6 @@
 package com.boomi.leavetrackingsystem.dao;
 
 import com.boomi.leavetrackingsystem.model.Attendance;
-import com.boomi.leavetrackingsystem.model.AttendanceCompositePK;
 
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
